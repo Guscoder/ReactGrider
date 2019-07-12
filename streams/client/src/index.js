3a1de815e361e0cd99bf1d726bb386a194811098
Justin Gustafson <jtwgus@gmail.com>
2019-07-12 08:47:23 -0400

    add redux dev tools to project

--- streams/client/src/index.js
+++ streams/client/src/index.js
@@ -1,18 +1,16 @@
 import React from "react";
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
 
-
-import App from './components/App';
-import reducers from './reducers';
-
+import App from "./components/App";
+import reducers from "./reducers";
 
 const store = createStore(reducers);
 
 ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
-    document.querySelector('#root')
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector("#root")
 );
